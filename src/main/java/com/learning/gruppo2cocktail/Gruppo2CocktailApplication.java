2c63737eea14a25d8fb55144aa3363a5ee3f336f
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2024-01-19 15:43:27 +0100

    initial commit

---
+++ src/main/java/com/learning/gruppo2cocktail/Gruppo2CocktailApplication.java
@@ -0,0 +1,13 @@
+package com.learning.gruppo2cocktail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gruppo2CocktailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gruppo2CocktailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/gruppo2cocktail/Gruppo2CocktailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.gruppo2cocktail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gruppo2CocktailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
