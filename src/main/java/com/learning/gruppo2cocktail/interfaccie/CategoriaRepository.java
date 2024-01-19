535b41cffad3c83417b276aa716497d457e0c60b
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2024-01-19 17:14:22 +0100

    extend repositories

--- src/main/java/com/learning/gruppo2cocktail/interfaccie/CategoriaRepository.java
+++ src/main/java/com/learning/gruppo2cocktail/interfaccie/CategoriaRepository.java
@@ -1,4 +1,7 @@
 package com.learning.gruppo2cocktail.interfaccie;
 
-public interface CategoriaRepository {
+import com.learning.gruppo2cocktail.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoriaRepository extends JpaRepository<Category, Integer> {
 }
