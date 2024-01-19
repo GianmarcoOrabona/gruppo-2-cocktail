3872bd04985471bc56e9d22f100ea419e07f1596
Gabriele02 <gagag@g,ail.com>
2024-01-19 17:44:08 +0100

    add Tamplete home and import sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+insert into cocktails (price, name, description, image) values (6, 'Mojito', 'Mojito cocktail', 'https://www.fattoincasadabenedetta.it/wp-content/uploads/2020/06/Mojito-copertina.jpg')
+INSERT INTO cocktails (price, name, description, image) VALUES (8, 'Manhattan', 'Manhattan cocktail', 'https://www.example.com/manhattan.jpg');
