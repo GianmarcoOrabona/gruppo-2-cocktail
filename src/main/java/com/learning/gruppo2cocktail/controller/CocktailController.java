8c2de5af5a6cd4da35d5d79c34c5690b80c49295
feffyx <ziacofederica@gmail.com>
2024-01-19 17:12:17 +0100

    controller

--- src/main/java/com/learning/gruppo2cocktail/controller/CocktailController.java
+++ src/main/java/com/learning/gruppo2cocktail/controller/CocktailController.java
@@ -1,5 +1,15 @@
 package com.learning.gruppo2cocktail.controller;
 
+import com.learning.gruppo2cocktail.model.Cocktail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
 @Controller
 @RequestMapping("/cocktails")
 public class CocktailController {
